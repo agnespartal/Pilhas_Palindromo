795b5a789618378b945aa9ed90b19fd9831f08b5
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-09-18 20:46:35 -0300

    Initial commit

---
+++ src/view/Principal.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import br.fateczl.pilhasString.PilhaString;
+import controller.PalindromoController;
+
+public class Principal {
+
+	public static void main(String[] args) throws Exception {
+		String palavra = JOptionPane.showInputDialog("Digite uma palavra");
+
+		PilhaString p = new PilhaString();
+		int tamanho = palavra.length();
+		for (int i = 0; i < tamanho; i++) {
+			p.push(palavra.substring(i, i + 1));
+		}
+
+		PalindromoController palin = new PalindromoController();
+		String palavraInvertida = palin.invertePalavra(p, tamanho);
+		boolean res = palin.comparaPalavra(palavra, palavraInvertida);
+		if (res) {
+			System.out.println("A palavra " + palavra + " é um palindromo");
+		} else {
+			System.out.println("A palavra " + palavra + " não é um palindromo");
+		}
+	}
+
+}
